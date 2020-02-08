33a857ecba1f9f843c0502a4f66615e52f22a313
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2020-02-08 19:31:04 +0100

    python3 way

--- test_create_dirs.py
+++ test_create_dirs.py
@@ -5,13 +5,10 @@ import os
 
 fname=sys.argv[1]
 
-dir_name = os.path.dirname(fname)
-if os.path.isdir(dir_name):
-  if not os.path.exists(dir_name):
-    print('Creating {}'.format(dir_name))
-    os.makedirs(dir_name)
-  else:
-    print('{} is already created'.format(dir_name))
+if '/' in fname:
+  dir_name = os.path.dirname(fname)
+  os.makedirs(dir_name, exist_ok=True) # python3 way
 
 with open(fname, 'w') as f:
   f.write('worked!')
+
