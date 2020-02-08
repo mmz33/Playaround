7b4cb300c81cb1360f0354bcad27f67326061863
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2020-02-08 19:14:38 +0100

    update for one file case

--- test_create_dirs.py
+++ test_create_dirs.py
@@ -6,11 +6,12 @@ import os
 fname=sys.argv[1]
 
 dir_name = os.path.dirname(fname)
-if not os.path.exists(dir_name):
-  print('Creating {}'.format(dir_name))
-  os.makedirs(dir_name)
-else:
-  print('{} is already created'.format(dir_name))
+if os.path.isdir(dir_name):
+  if not os.path.exists(dir_name):
+    print('Creating {}'.format(dir_name))
+    os.makedirs(dir_name)
+  else:
+    print('{} is already created'.format(dir_name))
 
 with open(fname, 'w') as f:
   f.write('worked!')
