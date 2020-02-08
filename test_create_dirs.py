f513722e670a543e8f26c115a154d8af98b665bc
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2020-02-08 19:12:35 +0100

    test create dirs

---
+++ test_create_dirs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+fname=sys.argv[1]
+
+dir_name = os.path.dirname(fname)
+if not os.path.exists(dir_name):
+  print('Creating {}'.format(dir_name))
+  os.makedirs(dir_name)
+else:
+  print('{} is already created'.format(dir_name))
+
+with open(fname, 'w') as f:
+  f.write('worked!')
